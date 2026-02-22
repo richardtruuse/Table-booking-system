72d534be69fca00f4ff0ed51631df7a9f52f67a4
richardtruuse <rtruus@taltech.ee>
2026-02-22 16:43:43 +0200

    initial file structure

---
+++ Frontend/app/components/layout/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer>
+   
+  </footer>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
---
+++ Frontend/app/components/layout/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header>
+   
+  </header>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
